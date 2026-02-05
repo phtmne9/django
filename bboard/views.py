16c708e0291f0b1952b1dfbbffbb8e73cb271024
Морозов Александр <@step.kz>
2026-02-05 18:44:27 +0500

    lesson_02: models

--- bboard/views.py
+++ bboard/views.py
@@ -1,5 +1,29 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
+
+from bboard.models import Bb
+
+
+# def index(request):
+#     s = 'Список объявлений\r\n\r\n\r\n'
+#
+#     for bb in Bb.objects.order_by('-published'):
+#         s += bb.title + '\r\n' + bb.content + '\r\n\r\n'
+#
+#     return HttpResponse(s, content_type='text/plain; charset=utf-8')
+
+
+# def index(request):
+#     template = loader.get_template('index.html')
+#     bbs = Bb.objects.order_by('-published')
+#     context = {'bbs': bbs}
+#
+#     return HttpResponse(template.render(context, request))
+
 
 def index(request):
-    return HttpResponse("Здесь будет выведен список объявлений.")
+    bbs = Bb.objects.order_by('-published')
+    context = {'bbs': bbs}
+
+    return render(request, 'index.html', context)
